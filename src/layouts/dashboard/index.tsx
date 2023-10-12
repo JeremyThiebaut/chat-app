3e131e92bf357e0a087fdc2649cb6b16c7669158
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-10-12 15:55:39 +0200

    Add synamic input

--- src/layouts/dashboard/index.tsx
+++ src/layouts/dashboard/index.tsx
@@ -1,13 +1,37 @@
 import { Outlet } from "react-router-dom";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import Divider from "@mui/material/Divider";
 import { useTheme } from "@mui/material/styles";
+import { ChatCircleDots, Gear, Phone, Users } from "phosphor-react";
+
+import Logo from "../../assets/images/logo.ico";
+import { useState } from "react";
+
+const Profil_Icon = () => [
+  {
+    index: 0,
+    icon: <ChatCircleDots />,
+  },
+  {
+    index: 1,
+    icon: <Users />,
+  },
+  {
+    index: 2,
+    icon: <Phone />,
+  },
+];
 
 const DashboardLayout = () => {
   const theme = useTheme();
-  console.log(theme);
+
+  const [selected, setSelected] = useState(0);
   return (
     <>
       <Box
+        p={2}
         sx={{
           backgroundColor: theme.palette.background.paper,
           boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
@@ -15,7 +39,77 @@ const DashboardLayout = () => {
           width: 100,
         }}
       >
-        Hello
+        <Stack
+          direction={"column"}
+          alignItems={"center"}
+          sx={{ width: "100%" }}
+          spacing={3}
+        >
+          <Box
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img src={Logo} alt={"Chat App Logo"} style={{ height: 64 }} />
+          </Box>
+          <Stack
+            sx={{ width: "max-content" }}
+            direction="column"
+            alignItems="center"
+            spacing={3}
+          >
+            {Profil_Icon().map((item) =>
+              item.index === selected ? (
+                <Box
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton
+                    sx={{ width: "max-content", color: "#fff" }}
+                    key={item.index}
+                  >
+                    {item.icon}
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => setSelected(item.index)}
+                  sx={{ width: "max-content", color: "#000" }}
+                  key={item.index}
+                >
+                  {item.icon}
+                </IconButton>
+              )
+            )}
+            <Divider sx={{ width: "48px" }} />
+            {selected === 3 ? (
+              <Box
+                sx={{
+                  backgroundColor: theme.palette.primary.main,
+                  borderRadius: 1.5,
+                }}
+              >
+                <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                  <Gear />
+                </IconButton>
+              </Box>
+            ) : (
+              <IconButton
+                onClick={() => {
+                  setSelected(3);
+                }}
+                sx={{ width: "max-content", color: "#000" }}
+              >
+                <Gear />
+              </IconButton>
+            )}
+          </Stack>
+        </Stack>
       </Box>
       <Outlet />
     </>

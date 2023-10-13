ee2e7cd6f0abfe63059280c823df42f593d867b3
Jeremy Thiebaut <jeremy@diggersfactory.com>
2023-10-13 17:11:30 +0200

    Adding Conversation into App

--- src/pages/dashboard/GeneralApp.tsx
+++ src/pages/dashboard/GeneralApp.tsx
@@ -1,10 +1,21 @@
+import { Box, Stack } from "@mui/material";
 import Chat from "./Chat";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
   return (
-    <>
+    <Stack direction={"row"} sx={{ with: "100%" }}>
       <Chat />
-    </>
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px)",
+          backgroundColor: "#fff",
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 

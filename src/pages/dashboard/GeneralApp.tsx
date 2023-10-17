a8456270c49e22331c28e31498086bec35466443
Jeremy Thiebaut <jeremy@diggersfactory.com>
2023-10-17 18:11:12 +0200

    Use reducer and add contact page

--- src/pages/dashboard/GeneralApp.tsx
+++ src/pages/dashboard/GeneralApp.tsx
@@ -1,20 +1,35 @@
 import { Box, Stack } from "@mui/material";
 import Chat from "./Chat";
 import Conversation from "../../components/Conversation";
+import Contact from "../../components/Contact";
+import { useSelector } from "react-redux";
+
+type GeneralAppProps = {
+  app: {
+    sidebar: {
+      open: boolean;
+    };
+  };
+};
 
 const GeneralApp = () => {
+  const { sidebar } = useSelector((state: GeneralAppProps) => state.app);
   return (
     <Stack direction={"row"} sx={{ with: "100%" }}>
+      {/* Chat */}
       <Chat />
       <Box
         sx={{
           height: "100%",
-          width: "calc(100vw - 420px)",
+          width: sidebar.open ? "calc(100vw - 740px)" : "calc(100vw - 420px)",
           backgroundColor: "#f0f4fa",
         }}
       >
+        {/* Conversation */}
         <Conversation />
       </Box>
+      {/* Contact */}
+      {sidebar.open && <Contact />}
     </Stack>
   );
 };

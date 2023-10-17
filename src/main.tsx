ec2cc50ff7af86122144b5a646368403fb678929
Jeremy Thiebaut <jeremy@diggersfactory.com>
2023-10-17 18:05:24 +0200

    Add provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ReduxProvider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReduxProvider>
   </React.StrictMode>
 );

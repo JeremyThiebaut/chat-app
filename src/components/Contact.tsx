4faa769127e2dbeb5ac39c897e1b83ea470bab74
Jeremy Thiebaut <jeremy@diggersfactory.com>
2023-10-17 18:05:39 +0200

    Make contact page

---
+++ src/components/Contact.tsx
@@ -0,0 +1,167 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { Bell, CaretRight, Phone, Star, VideoCamera, X } from "phosphor-react";
+import { useDispatch } from "../redux/store";
+import { ToggleSidebar } from "../redux/slices/app";
+import { randFirstName, randFullName, randImg } from "@ngneat/falso";
+import AntSwitch from "./AntSwitch";
+
+const Contact = () => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  return (
+    <Box sx={{ width: 320, height: "100vh" }}>
+      <Stack sx={{ height: "100%" }}>
+        {/* Header */}
+        <Box
+          sx={{
+            boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.2)",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#f8faff"
+                : theme.palette.background.default,
+          }}
+        >
+          <Stack
+            direction={"row"}
+            sx={{ height: "100%", width: "100%", p: 2 }}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            spacing={3}
+          >
+            <Typography variant="subtitle2">Contact Info</Typography>
+            <IconButton
+              onClick={() => {
+                dispatch(ToggleSidebar());
+              }}
+            >
+              <X />
+            </IconButton>
+          </Stack>
+        </Box>
+        {/* Body */}
+        <Stack
+          sx={{
+            height: "100%",
+            position: "relatice",
+            flexGrow: 1,
+            overflowY: "scroll",
+          }}
+          p={3}
+          spacing={3}
+        >
+          <Stack alignItems={"center"} direction={"row"} spacing={2}>
+            <Avatar
+              src={randImg()}
+              alt={randFirstName()}
+              sx={{ height: 64, width: 64 }}
+            />
+            <Stack spacing={0.5}>
+              <Typography variant="body1" fontWeight={600}>
+                {randFullName()}
+              </Typography>
+              <Typography variant="body2" fontWeight={500}>
+                {"+91 729 2829 2992"}
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-evenly"}
+          >
+            <Stack spacing={1} alignItems={"center"}>
+              <IconButton>
+                <Phone />
+              </IconButton>
+              <Typography variant="overline">Voice</Typography>
+            </Stack>
+            <Stack spacing={1} alignItems={"center"}>
+              <IconButton>
+                <VideoCamera />
+              </IconButton>
+              <Typography variant="overline">Video</Typography>
+            </Stack>
+          </Stack>
+          <Divider />
+          <Stack spacing={0.5}>
+            <Typography variant="body1">About</Typography>
+            <Typography variant="body2">
+              Imagination is the only limit
+            </Typography>
+          </Stack>
+          <Divider />
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Typography variant="subtitle2">Media, Links & Docks</Typography>
+            <Button endIcon={<CaretRight />}>401</Button>
+          </Stack>
+          <Stack direction={"row"} spacing={2} alignItems={"center"}>
+            {[1, 2, 3].map((_, index) => (
+              <Box key={index}>
+                <img
+                  src={randImg()}
+                  alt={randFirstName()}
+                  style={{ width: "100%" }}
+                />
+              </Box>
+            ))}
+          </Stack>
+          <Divider />
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Stack direction={"row"} spacing={2} alignItems={"center"}>
+              <Star size={21} />
+              <Typography variant="subtitle2">Starred Messages</Typography>
+            </Stack>
+            <IconButton>
+              <CaretRight />
+            </IconButton>
+          </Stack>
+          <Divider />
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Stack direction={"row"} spacing={2} alignItems={"center"}>
+              <Bell size={21} />
+              <Typography variant="subtitle2">Mute Notifications</Typography>
+            </Stack>
+            <AntSwitch />
+          </Stack>
+          <Divider />
+          <Typography>1 group in common</Typography>
+          <Stack direction={"row"} spacing={2} alignItems={"center"}>
+            <Avatar src={randImg()} alt={randFirstName()} />
+            <Stack spacing={0.5}>
+              <Typography variant="subtitle2">Coding Monk</Typography>
+              <Typography variant="caption">
+                Owl, Parrot, Rabbit, You
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Contact;

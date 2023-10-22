ba36d927473b8ae020284b24da7c52b2f8233ced
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-10-22 19:12:52 +0200

    Add more routes of app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { useRoutes, Navigate } from "react-router-dom";
-import DashboardLayout from "../layouts/dashboard";
+import DashboardLayout from "@/layouts/dashboard";
+import MainLayout from "@/layouts/main";
 import { Suspense, lazy } from "react";
-import LoadingScreen from "../components/LoadingScreen";
+import LoadingScreen from "@/components/LoadingScreen";
 
 const Loadable = (Component: any) => (props: any) => {
   return (
@@ -11,20 +12,35 @@ const Loadable = (Component: any) => (props: any) => {
   );
 };
 
-const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp"))
-);
-
-const Page404 = Loadable(lazy(() => import("../pages/Page404")));
-
 const Router = () => {
   return useRoutes([
+    {
+      path: "/auth",
+      element: <MainLayout />,
+      children: [
+        { path: "login", element: <LoginPage /> },
+        {
+          path: "register",
+          element: <RegisterPage />,
+        },
+        {
+          path: "reset-password",
+          element: <ResetPasswordPage />,
+        },
+        {
+          path: "new-password",
+          element: <NewPasswordPage />,
+        },
+      ],
+    },
     {
       path: "/",
       element: <DashboardLayout />,
       children: [
         { element: <Navigate to="/" replace />, index: true },
         { path: "app", element: <GeneralApp /> },
+        { path: "settings", element: <Settings /> },
+        { path: "group", element: <GroupPage /> },
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
@@ -32,5 +48,20 @@ const Router = () => {
     { path: "*", element: <Navigate to="/404" replace /> },
   ]);
 };
+
+const GeneralApp = Loadable(
+  lazy(() => import("../pages/dashboard/GeneralApp"))
+);
+const Settings = Loadable(lazy(() => import("@/pages/dashboard/Settings")));
+const LoginPage = Loadable(lazy(() => import("@/pages/auth/Login")));
+const RegisterPage = Loadable(lazy(() => import("@/pages/auth/Register")));
+const ResetPasswordPage = Loadable(
+  lazy(() => import("@/pages/auth/ResetPassword"))
+);
+const NewPasswordPage = Loadable(
+  lazy(() => import("@/pages/auth/NewPassword"))
+);
+const GroupPage = Loadable(lazy(() => import("@/pages/dashboard/Group")));
+const Page404 = Loadable(lazy(() => import("@/pages/Page404")));
 
 export default Router;

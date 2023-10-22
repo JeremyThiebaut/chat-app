ad5085f96a26bc73aa112cd8558786c7025dbe9d
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-10-22 18:43:18 +0200

    Make settings of i18n

---
+++ src/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import fr from "@/locales/fr/app.json";
+import en from "@/locales/en/app.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  fr: {
+    translation: fr,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: "en",
+  resources,
+});
+
+export default i18n;

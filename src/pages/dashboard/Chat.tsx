7696432b01e8fca7b0489e5ff854eb8d231de1dc
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-10-22 19:03:49 +0200

    Factorized function

--- src/pages/dashboard/Chat.tsx
+++ src/pages/dashboard/Chat.tsx
@@ -1,127 +1,19 @@
 import {
-  Avatar,
   Box,
   Button,
   Divider,
   IconButton,
-  InputBase,
   Stack,
   Typography,
-  alpha,
-  styled,
-  Badge,
 } from "@mui/material";
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
-import { ChatList } from "../../data";
-import { randImg } from "@ngneat/falso";
-
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  "& .MuiBadge-badge": {
-    backgroundColor: "#44b700",
-    color: "#44b700",
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    "&::after": {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "75%",
-      height: "75%",
-      borderRadius: "50%",
-      animation: "ripple 1.2s infinite ease-in-out",
-      border: "1px solid currentColor",
-      content: '""',
-    },
-  },
-  "@keyframes ripple": {
-    "0%": {
-      transform: "scale(.8)",
-      opacity: 1,
-    },
-    "100%": {
-      transform: "scale(2.4)",
-      opacity: 0,
-    },
-  },
-}));
-
-const ChatElement = (params: {
-  id: number;
-  name: string;
-  img: string;
-  msg: string;
-  time: string;
-  unread: number;
-  online: boolean;
-}) => {
-  return (
-    <Box
-      sx={{
-        width: "calc(100% - 32px)",
-        borderRadius: 1,
-        backgroundColor: "#fff",
-      }}
-      p={2}
-    >
-      <Stack
-        direction="row"
-        alignItems={"center"}
-        justifyContent="space-between"
-      >
-        <Stack direction="row" spacing={2}>
-          {params.online ? (
-            <StyledBadge
-              overlap="circular"
-              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-              variant="dot"
-            >
-              <Avatar alt="Remy Sharp" src={randImg()} />
-            </StyledBadge>
-          ) : (
-            <Avatar alt="Remy Sharp" src={randImg()} />
-          )}
-          <Stack spacing={0.3}>
-            <Typography variant="subtitle2">{params.name}</Typography>
-            <Typography variant="caption">{params.msg}</Typography>
-          </Stack>
-        </Stack>
-        <Stack spacing={2} alignItems={"center"}>
-          <Typography sx={{ fontWeight: 600 }} variant="caption">
-            {params.time}
-          </Typography>
-          <Badge color="primary" badgeContent={params.unread}></Badge>
-        </Stack>
-      </Stack>
-    </Box>
-  );
-};
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: 20,
-  backgroundColor: alpha(theme.palette.background.paper, 1),
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    width: "100%",
-  },
-}));
+import { ChatList } from "@/data";
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "@/components/Search";
+import ChatElement from "@/components/ChatElement";
 
 const Chat = () => {
   return (

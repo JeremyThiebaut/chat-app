90015b3027cfdf8ba3d65f347e0ef6b6199f01a1
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-10-22 18:42:52 +0200

    Add translation changer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import Router from "./routes";
-function App() {
+import React from "react";
+import Router from "@/routes";
+import { useTranslation } from "react-i18next";
+import { userSelector } from "@/redux/store";
+
+type PropsLanguage = {
+  app: {
+    language: string;
+  };
+};
+
+const App: React.FC = () => {
+  const { i18n } = useTranslation();
+  const language = userSelector((state: PropsLanguage) => state.app.language);
+
+  React.useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [i18n, language]);
+
   return <Router />;
-}
+};
 
 export default App;
